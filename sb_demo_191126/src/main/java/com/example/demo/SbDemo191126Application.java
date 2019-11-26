5bf4e8c8895a7144b19ae3820b11a98da96cba05
Luck <Luck>
2019-11-26 16:10:19 +0800

    test

---
+++ sb_demo_191126/src/main/java/com/example/demo/SbDemo191126Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDemo191126Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDemo191126Application.class, args);
+	}
+
+}
